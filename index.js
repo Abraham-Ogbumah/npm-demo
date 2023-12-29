7ac6cf9bd708433c70c78297085516bd52281571
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-12-29 17:16:56 -0500

    added PUT and updated POST

--- index.js
+++ index.js
@@ -19,14 +19,10 @@ app.get('/api/courses', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
+    const { error } = validateCourse(req.body)
 
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) {
-        res.status(400).send(result.error)
+    if (error) {
+        res.status(400).send(error)
         return;
     }
 
@@ -38,11 +34,34 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 })
 
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send('The course with the given id is not found')
+
+    const { error } = validateCourse(req.body)
+
+    if (error) {
+        res.status(400).send(error)
+        return;
+    }
+
+    course.name = req.body.name
+    res.send(course)
+})
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
     if (!course) res.status(404).send('The course with the given id is not found')
     res.send(course)
 })
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
